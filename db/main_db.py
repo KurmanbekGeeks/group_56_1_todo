64a24abcd783ded1f9718501dd899542660d7090
Kurmanbek-1 <kurmanbekmomunkulov32@gmail.com>
2025-08-19 09:58:45 +0600

    lesson 5

---
+++ db/main_db.py
@@ -0,0 +1,47 @@
+import sqlite3
+from db import queries
+from config import db_path
+
+
+def init_db():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(queries.CREATE_TASK)
+    print('База данных подключена!')
+    conn.commit()
+    conn.close()
+
+
+def get_tasks():
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(queries.SELECT_TASKS)
+    tasks = cursor.fetchall()
+    conn.close()
+    return tasks
+
+
+def add_task(task):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(queries.INSERT_TASK, (task,))
+    conn.commit()
+    task_id = cursor.lastrowid
+    conn.close()
+    return task_id
+
+
+def delete_task(task_id):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(queries.DELETE_TASK, (task_id,))
+    conn.commit()
+    conn.close()
+
+
+def update_task(task_id, new_task):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute(queries.UPDATE_TASK, (new_task, task_id))
+    conn.commit()
+    conn.close()
---
+++ db/queries.py
@@ -0,0 +1,14 @@
+CREATE_TASK = """
+    CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task TEXT NOT NULL
+    )
+"""
+
+SELECT_TASKS = "SELECT id, task FROM tasks"
+
+UPDATE_TASK = "UPDATE tasks SET task = ? WHERE id = ?"
+
+DELETE_TASK = "DELETE FROM tasks WHERE id = ?"
+
+INSERT_TASK = "INSERT INTO tasks (task) VALUES (?)"
